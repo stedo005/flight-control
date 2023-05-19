b7c44c99ea1fb7de45767d9a77826f34f66ab5ee
Steve Dölgner <steve.doelgner@gmail.com>
2023-05-19 11:35:47 +0200

    add hello

---
+++ frontend/src/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      Hallo
+    </div>
+  );
+}
+
+export default Home;
