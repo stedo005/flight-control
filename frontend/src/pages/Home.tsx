1c9c950ef4775a117fb9e72081b8257156b69487
Steve Dölgner <steve.doelgner@gmail.com>
2023-05-19 14:02:10 +0200

    refoctoring

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import '../App.css';
+
+function Home() {
+
+  interface Pilot {
+    id: string
+    name: string
+  }
+
+  const [pilots, setPilots] = useState([] as Array<Pilot>)
+
+  const fetchPilots = () => {
+    fetch("http://localhost:8080/api/admin/all-pilots", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then((responseBody: Array<Pilot>) => setPilots(responseBody))
+  }
+
+  const createPilot = () => {
+    fetch("http://localhost:8080/api/admin/create-pilot", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+  }
+
+  return (
+    <div>
+      <p>Hallo</p>
+      <button onClick={createPilot}>neuer Pilot</button>
+      <button onClick={fetchPilots}>alle Piloten</button>
+      {
+        pilots.map(pilot => <p>{pilot.name}</p>)
+      }
+    </div>
+  );
+}
+
+export default Home;
